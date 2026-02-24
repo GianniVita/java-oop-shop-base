9f7fe44fd8b3c55d68ce91dbf4f63bf77d7a4059
giannivitale <giavitale78@gmail.com>
2026-02-24 09:51:18 +0100

    Refactor Product class and update Main application

--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -12,13 +12,36 @@ public class Product {
     private double price;
     private double vat;
 
-    public Product(int code,String name, String brand, double price, double vat) {
+    Product(int code,String name, String brand, double price, double vat) {
         Random rand = new Random();
         this.code = rand.nextInt(999999);
         this.name = name;
         this.brand = brand;
         this.price = price;
         this.vat = vat;
+    }
+
+    public int getCode(){
+        return code;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getBrand(){
+        return brand;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+    public double getVat(){
+        return vat;
+     }
+
+    public void setCode(int code){
+        this.code = code;
     }
 
 }
